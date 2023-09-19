cb1a460f36155cc177a0e52b96ca2a4de4248299
milan460 <patel.milan416@gmail.com>
2023-09-19 18:57:08 -0400

    change functionality

--- src/main/java/com/example/StockWeb/model/StockDTO.java
+++ src/main/java/com/example/StockWeb/model/StockDTO.java
@@ -3,12 +3,81 @@ package com.example.StockWeb.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class StockDTO {
     @Getter
     @Setter
-    private int price;
+    private int regularMarketPrice;
+
+    @Getter
+    @Setter
+    private double postMarketPrice;
+
+    @Getter
+    @Setter
+    private double prevClosePrice;
+
+    @Getter
+    @Setter
+    private double openPrice;
+
+    @Getter
+    @Setter
+    private String bid;
+
+    @Getter
+    @Setter
+    private String ask;
+
+    @Getter
+    @Setter
+    private String dayRange;
+
+    @Getter
+    @Setter
+    private String fiftyTwoWeekRange;
+
+    @Getter
+    @Setter
+    private double volume;
+
+    @Getter
+    @Setter
+    private double avgVolume;
+
+    @Getter
+    @Setter
+    private String marketCap;
+
+    @Getter
+    @Setter
+    private double beta;
+
+    @Getter
+    @Setter
+    private double PERatio;
+
+    @Getter
+    @Setter
+    private double EPS;
+
+    @Getter
+    @Setter
+    private String earningsData;
+
+    @Getter
+    @Setter
+    private String forwardDividendAndYield;
+
+    @Getter
+    @Setter
+    private String exDividendDate;
+
+    @Getter
+    @Setter
+    private double oneYearTarget;
 
     @Getter
     @Setter
@@ -16,9 +85,26 @@ public class StockDTO {
 
     public StockDTO(){}
 
-    public StockDTO(int price, String name){
+    public StockDTO(int regularMarketPrice, double postMarketPrice, double prevClosePrice, double openPrice, String bid, String ask, String dayRange, String fiftyTwoWeekRange, double volume, double avgVolume, String marketCap, double beta, double PERatio, double EPS, String earningsData, String forwardDividendAndYield, String exDividendDate, double oneYearTarget, String name) {
+        this.regularMarketPrice = regularMarketPrice;
+        this.postMarketPrice = postMarketPrice;
+        this.prevClosePrice = prevClosePrice;
+        this.openPrice = openPrice;
+        this.bid = bid;
+        this.ask = ask;
+        this.dayRange = dayRange;
+        this.fiftyTwoWeekRange = fiftyTwoWeekRange;
+        this.volume = volume;
+        this.avgVolume = avgVolume;
+        this.marketCap = marketCap;
+        this.beta = beta;
+        this.PERatio = PERatio;
+        this.EPS = EPS;
+        this.earningsData = earningsData;
+        this.forwardDividendAndYield = forwardDividendAndYield;
+        this.exDividendDate = exDividendDate;
+        this.oneYearTarget = oneYearTarget;
         this.name = name;
-        this.price = price;
     }
 
     @Override
@@ -26,18 +112,35 @@ public class StockDTO {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         StockDTO stockDTO = (StockDTO) o;
-        return price == stockDTO.price && Objects.equals(name, stockDTO.name);
+        return regularMarketPrice == stockDTO.regularMarketPrice && Double.compare(postMarketPrice, stockDTO.postMarketPrice) == 0 && Double.compare(prevClosePrice, stockDTO.prevClosePrice) == 0 && Double.compare(openPrice, stockDTO.openPrice) == 0 && Double.compare(volume, stockDTO.volume) == 0 && Double.compare(avgVolume, stockDTO.avgVolume) == 0 && Double.compare(beta, stockDTO.beta) == 0 && Double.compare(PERatio, stockDTO.PERatio) == 0 && Double.compare(EPS, stockDTO.EPS) == 0 && Double.compare(oneYearTarget, stockDTO.oneYearTarget) == 0 && Objects.equals(bid, stockDTO.bid) && Objects.equals(ask, stockDTO.ask) && Objects.equals(dayRange, stockDTO.dayRange) && Objects.equals(fiftyTwoWeekRange, stockDTO.fiftyTwoWeekRange) && Objects.equals(marketCap, stockDTO.marketCap) && Objects.equals(earningsData, stockDTO.earningsData) && Objects.equals(forwardDividendAndYield, stockDTO.forwardDividendAndYield) && Objects.equals(exDividendDate, stockDTO.exDividendDate) && Objects.equals(name, stockDTO.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(price, name);
+        return Objects.hash(regularMarketPrice, postMarketPrice, prevClosePrice, openPrice, bid, ask, dayRange, fiftyTwoWeekRange, volume, avgVolume, marketCap, beta, PERatio, EPS, earningsData, forwardDividendAndYield, exDividendDate, oneYearTarget, name);
     }
 
     @Override
     public String toString() {
         return "StockDTO{" +
-                "price=" + price +
+                "regularMarketPrice=" + regularMarketPrice +
+                ", postMarketPrice=" + postMarketPrice +
+                ", prevClosePrice=" + prevClosePrice +
+                ", openPrice=" + openPrice +
+                ", bid='" + bid + '\'' +
+                ", ask='" + ask + '\'' +
+                ", dayRange='" + dayRange + '\'' +
+                ", fiftyTwoWeekRange='" + fiftyTwoWeekRange + '\'' +
+                ", volume=" + volume +
+                ", avgVolume=" + avgVolume +
+                ", marketCap='" + marketCap + '\'' +
+                ", beta=" + beta +
+                ", PERatio=" + PERatio +
+                ", EPS=" + EPS +
+                ", earningsData='" + earningsData + '\'' +
+                ", forwardDividendAndYield='" + forwardDividendAndYield + '\'' +
+                ", exDividendDate='" + exDividendDate + '\'' +
+                ", oneYearTarget=" + oneYearTarget +
                 ", name='" + name + '\'' +
                 '}';
     }
--- src/main/java/com/example/StockWeb/service/StockInformationService.java
+++ src/main/java/com/example/StockWeb/service/StockInformationService.java
@@ -1,5 +1,6 @@
 package com.example.StockWeb.service;
 
+import com.example.StockWeb.StockWebApplication;
 import com.example.StockWeb.model.StockDTO;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
@@ -10,23 +11,64 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
 
 
 @Service
 public class StockInformationService implements StockService{
 
+    private static final String baseURL = "https://finance.yahoo.com/quote/";
+    private static final String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36";
+    static Logger logger = Logger.getLogger(StockInformationService.class.getName());
     @Override
     public StockDTO getStockTicker(String tickerSymbol) {
+
         return null;
     }
 
-    private void extractDataFromCnn(){
+    private void extractStockDataFromYahoo(String stockTicker){
+        StockDTO stockInfo = new StockDTO();
         try{
-            //loading the HTML to a Document Object
-            Document document = Jsoup.connect(url).get();
+            //connecting and getting the HTML from the URL
+            Document doc = Jsoup.connect(baseURL.concat(stockTicker + "?p=" + stockTicker)).userAgent(userAgent).get();
 
-            //Select the element that contains the stock price information
-            Element element = document.get
+            //getting the name of the stock from the h1 tag
+            Element companyNameElement = doc.getElementsByTag("h1").first();
+            stockInfo.setName(companyNameElement.text());
+            logger.info(companyNameElement.text());
+
+            //getting the stock price data from the tag
+            Elements priceData = doc.getElementsByAttributeValue("data-test", "qsp-price");
+            //get the regular market price
+            stockInfo.setRegularMarketPrice(Integer.parseInt(priceData.get(0).text()));
+
+            //get the post market price
+            stockInfo.setPostMarketPrice(Integer.parseInt(priceData.get(1).text()));
+            logger.info(priceData.text());
+
+            Elements leftTable = doc.getElementsByAttributeValue("data-test", "left-summary-table").get(0).getElementsByTag("tr");
+
+            HashMap<String, String> leftTableData = new HashMap<>();
+            for (Element element : leftTable) {
+                String title = element.getElementsByTag("td").get(0).text();
+                String value = element.getElementsByTag("td").get(1).text();
+                leftTableData.put(title, value);
+            }
+
+
+            Elements rightTable = doc.getElementsByAttributeValue("data-test", "right-summary-table").get(0).getElementsByTag("tr");
+
+            HashMap<String, String> rightTableData = new HashMap<>();
+
+            for(Element element: rightTable){
+                String title = element.getElementsByTag("td").get(0).text();
+                String value = element.getElementsByTag("td").get(1).text();
+                rightTableData.put(title, value);
+            }
+
+
         }
         catch(IOException ex){
             ex.printStackTrace();
